02c63a3a55fc9612ef536b4e1de0588eb855b712
MNprogramming <142527849+mnprogramming@users.noreply.github.com>
2024-08-26 16:20:27 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,8 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID|| 'EDlHRS6S#UbjLPDId9EpgdkwbNhOHHxmHNP_L_O0BeGqXHZqdNWs',
 ALIVE_IMG:  process.env.ALIVE_IMG || 'https://telegra.ph/file/de735f458933d1601b361.png',
-ALIVE_MSG:  process.env.ALIVE_MSG || '🔰methukaMD🐱‍👓🔰 is alive now'    
+ALIVE_MSG:  process.env.ALIVE_MSG || '🔰methukaMD🐱‍👓🔰 is alive now
+    💻-Methuka Nisalitha 🚀The fastest WA BOT'    
 };
